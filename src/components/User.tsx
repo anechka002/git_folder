c716c15496aae4210151feb562633c8989029dd3
Anna <blinova.anneta@gmail.com>
2024-02-23 10:56:30 +0100

    add text for User component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 function User() {
   return (
-    <div>User</div>
-  )
+    <div>
+      <div>User</div>
+      <p>text</p>
+    </div>
+  );
 }
 
-export default User
+export default User;
